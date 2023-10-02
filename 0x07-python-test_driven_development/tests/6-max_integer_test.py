5d9f2446005536cfa78d8003771b88a7f0d5a4b0
josiah-mbao <josiahmbaomc@gmail.com>
2023-10-02 12:02:16 -0700

    fixed 6.

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 # 6-max_integer_test.py
-"""The Unittests for max_integer([..])."""
+"""Unittests for max_integer([..])."""
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Defines unittests for max_integer([..])."""
+    """Defines the unittests for max_integer([..])."""
 
     def test_ordered_list(self):
-        """Test an ordered list of integers."""
+        """Tests an ordered list of integers."""
         ordered = [1, 2, 3, 4]
         self.assertEqual(max_integer(ordered), 4)
 
     def test_unordered_list(self):
-        """Test an unordered list of integers."""
+        """Tests an unordered list of integers."""
         unordered = [1, 2, 4, 3]
         self.assertEqual(max_integer(unordered), 4)
 
@@ -60,6 +60,3 @@ class TestMaxInteger(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-+++++++++++++++++++++++++++++++++
